d6a57dff027a2d33a0c8d6a505049d236379b74f
ayush <ayush@AYUSHGANDHI>
2024-02-17 17:03:26 +0530

    Add project files.

---
+++ salary count make class object/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salary_count_make_class_object
+{
+    public class Employee
+    {
+        int id = 0;
+        string name = "";
+        int salary = 0;
+
+        public void input()
+        {
+            Console.WriteLine("Please Enter ID:");
+            id=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Name:");
+            name=Console.ReadLine();
+
+            Console.WriteLine("Please Enter Salary:");
+            salary=Convert.ToInt32(Console.ReadLine());
+        }
+        public void output() 
+        {
+            Console.WriteLine(id);
+            Console.WriteLine(name);
+            Console.WriteLine("The salary is:"+salary);
+
+            int hra = Convert.ToInt32(salary * 0.5);
+            Console.WriteLine("The hra is:" + hra);
+
+            int da=Convert.ToInt32(salary * 0.2);
+            Console.WriteLine("The DA is:" + da);
+
+            int totalsalary = salary + hra + da;
+            Console.WriteLine("The Totalsalary is:" + totalsalary);
+
+            int tax=Convert.ToInt32(totalsalary * 0.1);
+            Console.WriteLine("The tax is:"+tax);
+
+            int netsalary=Convert.ToInt32(totalsalary -tax);
+            Console.WriteLine("The netsalary is:"+netsalary);
+
+        }
+    }
+}
---
+++ salary count make class object/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salary_count_make_class_object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee ayu = new Employee();
+
+            ayu.input();
+            ayu.output();
+            Console.ReadKey();
+        }
+    }
+}
